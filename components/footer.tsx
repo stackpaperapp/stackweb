7d48007c036a8fa9bb44c45fb3d2a3abb4f51004
Mager <andrew.mager@gmail.com>
2023-05-14 08:08:04 -0500

    Start footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,28 +1,44 @@
-import Link from "next/link"
-import styles from "./footer.module.css"
-import packageJSON from "../package.json"
+import Image from "next/image";
+import { useTheme } from "next-themes";
 
-export default function Footer() {
+const Footer = () => {
+  const { resolvedTheme } = useTheme();
+
   return (
-    <footer className={styles.footer}>
-      <hr />
-      <ul className={styles.navItems}>
-        <li className={styles.navItem}>
-          <a href="https://next-auth.js.org">Documentation</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://www.npmjs.com/package/next-auth">NPM</a>
-        </li>
-        <li className={styles.navItem}>
-          <a href="https://github.com/nextauthjs/next-auth-example">GitHub</a>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/policy">Policy</Link>
-        </li>
-        <li className={styles.navItem}>
-          <em>next-auth@{packageJSON.dependencies["next-auth"]}</em>
-        </li>
-      </ul>
+    <footer>
+      <div className="flex items-center justify-center flex-col my-4">
+        <div className="flex items-center mb-4">
+          <div className="mx-2">
+            <a href="https://twitter.com/stackpaperapp">
+              <Image
+                src={`/twitter-${resolvedTheme}.svg`}
+                alt="Twitter"
+                width={32}
+                height={32}
+                priority
+              />
+            </a>
+          </div>
+          <div className="mx-2">
+            <a href="https://github.com/stackpaperapp">
+              <Image
+                src={`/github-${resolvedTheme}.svg`}
+                alt="Github"
+                width={32}
+                height={32}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+        <div>
+          <p className="text-sm italic">
+            Made in Chicago by <a href="https://twitter.com/mager">@mager</a>
+          </p>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
+
+export default Footer;
